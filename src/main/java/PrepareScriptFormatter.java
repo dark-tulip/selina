fc0a13468e4e56f5761903947b594663b6f251b0
dark-tulip <t.telbayeva@mail.ru>
2023-04-26 09:52:09 +0600

    started formatter for sql payload

---
+++ src/main/java/PrepareScriptFormatter.java
@@ -0,0 +1,10 @@
+import org.postgresql.core.Field;
+
+import java.io.File;
+
+public class PrepareScriptFormatter {
+
+  public static void main(String[] args) {
+    File sqlPayload = new File("");
+  }
+}
